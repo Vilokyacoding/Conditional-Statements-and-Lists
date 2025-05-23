dfe14db2d93fc68ff2df47de0094045b51417948
Vilokyacoding <vilokya2025@gmail.com>
2025-05-23 07:29:18 -0400

    Odd and even numbers

---
+++ odd even numbers.py
@@ -0,0 +1,6 @@
+number=int(input("Enter a number to check :"))
+print("Number to be checked  :", number)
+if number % 2 == 0:
+    print("The number is even")
+elif number % 2 == 1:
+    print("The number is odd")
