6e1a9c7aefe9f097ff59c9821f51ada7f4609561
Vilokyacoding <vilokya2025@gmail.com>
2025-05-23 20:47:42 -0400

    Homework

---
+++ Homework.py
@@ -0,0 +1,8 @@
+number= int(input("Enter a number to check :") )
+print("Number to be checked is :", number)
+if number > 2:
+    print("The number is positive")
+elif number < -1:
+    print("The number is negative")
+else:
+    print("This is neutral")
